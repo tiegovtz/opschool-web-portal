06d95cae9b07570d08efadf7388f9972be80506e
Cyden-Mnara <mnarasaid7@gmail.com>
2025-03-06 09:13:05 +0300

    Hello Mr. Minja Please take your time to review this 	modified:   .nuxt/components.d.ts 	modified:   .nuxt/imports.d.ts 	modified:   .nuxt/nuxt-icon-server-bundle.mjs 	modified:   .nuxt/nuxt.d.ts 	modified:   .nuxt/tsconfig.json 	modified:   .nuxt/types/build.d.ts 	modified:   .nuxt/types/imports.d.ts 	modified:   .nuxt/types/nitro-routes.d.ts 	modified:   .nuxt/types/plugins.d.ts 	modified:   assets/css/main.css 	modified:   components/home/TopicCard.vue 	new file:   components/loading/loadingIndicator.vue 	modified:   pages/audio/index.vue 	modified:   pages/home/index.vue 	modified:   pages/interactive/[topicId].vue 	modified:   pages/printed/index.vue 	modified:   pages/video/index.vue 	new file:   server/api/get-books.js 	new file:   utilities/extract.js

--- pages/audio/index.vue
+++ pages/audio/index.vue
@@ -2,11 +2,15 @@
   <div class="container">
     <HeroSection />
     <TabBar />
-    <h1>Audio</h1>
+    <div class="loading flex justify-center" >
+      <LoadingIndicator :is-loading="true" />
+    </div>
+    <!-- render books -->
   </div>
 </template>
 
 <script setup>
 import HeroSection from '@/components/home/HeroSection.vue'
 import TabBar from '@/components/home/TabBar.vue'
+
 </script>
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,4 +1,3 @@
-
 <script setup>
 import TopicCard from "@/components/home/TopicCard.vue";
 import HeroSection from '@/components/home/HeroSection.vue'
@@ -7,41 +6,63 @@ import { ref, computed, onMounted } from 'vue';
 import { isGreaterToXL, isGreaterToLG, isGreaterToMD, isGreaterToSM, screenWidth } from '@/utilities/controlls';
 import InputsSelection from '@/components/home/InputsSelection.vue'
 
-const { data, status, error } = useFetch("/api/get-topic", {
+const { data: topic, status, error } = useFetch("/api/get-topic", {
   query: { streamId: 1, subjectId: 3 },
 });
 
-  // current page data
-  const currentPage = ref(1);
-  const pageSize = ref(isGreaterToXL.value ? 12 : isGreaterToLG.value ? 9 : isGreaterToMD.value ? 6 : isGreaterToSM.value ? 4 : 4);
+// current page data
+const currentPage = ref(1);
+const pageSize = ref();
+
+//  assigning page size based on screen sizes
+if (isGreaterToXL) {
+  pageSize.value = 12; // 12 topic cards
+}
+else if (isGreaterToLG) {
+  pageSize.value = 9; // 9 topic cards
+}
+else if (isGreaterToMD) {
+  pageSize.value = 6; // 6 topic cards per page
+}
+else {
+  pageSize.value = 4;// 4 topics card per page
+}
 
 
-  // total pages data
-  const totalPages = computed(() => {
-    return Math.ceil(data.value.data.length / pageSize.value);
-  });
+// total pages data
+const totalPages = computed(() => {
+  if (topic.value) {
+    return Math.ceil(topic.value.data.length / pageSize.value);
+  } else {
+    return 0; // return 0 if no data
+  }
+});
 
-  // extact total pages in groups of 5
+// extact total pages in groups of 5
 
 
-  // slice data to 9
-  const slicedData = ref();
-  const sliceData = (start, end) => {
-    slicedData.value = data.value.data.slice(start, end);
-  };
+// slice data to 9
+const slicedData = ref();
+const sliceData = (start, end) => {
+  if (topic.value?.data) {
+    slicedData.value = topic.value.data.slice(start, end);
+  }
+};
 
+sliceData(
+  (currentPage.value - 1) * pageSize.value,
+  currentPage.value * pageSize.value
+)
 // On mount, always slice with initial 12 items
-onMounted(() => {
-  sliceData(
-    (currentPage.value - 1) * pageSize.value, 
-    currentPage.value * pageSize.value
-  )
-})
+// onMounted(() => {
+
+// })
 
 // Watch screen width and update page size accordingly
 watch(() => screenWidth.value, () => {
   if (screenWidth.value >= 1280) {
-    pageSize.value = 12
+    pageSize.value = 12;
+
   } else if (screenWidth.value >= 1024 && screenWidth.value < 1280) {
     pageSize.value = 9
   } else if (screenWidth.value >= 768 && screenWidth.value < 1024) {
@@ -51,7 +72,14 @@ watch(() => screenWidth.value, () => {
   } else {
     pageSize.value = 4
   }
-}, { immediate: true });
+
+  // slice data per page size
+  sliceData(
+    (currentPage.value - 1) * pageSize.value,
+    currentPage.value * pageSize.value
+  )
+});
+
 
 // once pages are more than 5, handle pagination
 const nextPage = () => {
@@ -73,9 +101,12 @@ const prevPage = () => {
     <TabBar />
     <InputsSelection />
 
-    <div v-if="status === 'pending'" class="flex justify-center items-center">Loading...</div>
+    <div v-if="status === 'pending'" class="flex justify-center items-center">
+      <LoadingIndicator :is-loading="true" />
+
+    </div>
     <div v-else-if="status === 'error'">Error: {{ error.message }}</div>
-    <div v-else-if="status === 'success'">
+    <div v-else-if="status == 'success'">
       <div class=" grid grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-6 mb-10">
         <TopicCard v-for="topic in slicedData" :key="topic.topic_id" :topic-id="topic.topic_id"
           :topic-image="'https://kisomo.co.tz/kisomo app/' + topic.img_path" :topic-title="topic.topic_title"
@@ -108,9 +139,41 @@ const prevPage = () => {
              </div>
         </div>
       </div>
+
     </div>
     <div v-else>
-      <p>Try to refresh the page</p>
+      <div class="" v-if="sliceData.length === 0">Try to refresh the page, Something went Wrong</div>
+      <div class=" grid grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-6 mb-10" v-else>
+        <TopicCard v-for="topic in slicedData" :key="topic.topic_id" :topic-id="topic.topic_id"
+          :topic-image="'https://kisomo.co.tz/kisomo app/' + topic.img_path" :topic-title="topic.topic_title"
+          :topic-description="topic.topic_description"
+          :topic-duration="topic.topic_duration ? topic.topic_duration : '10 min'"
+          :topic-likes="topic.topic_likes ? topic.topic_likes : 100" :topic-views="topic.views ? topic.views : 100" />
+
+        <!-- pagination numbers based on data length greater to 9 -->
+        <div class="flex justify-center mb-10">
+          <div v-if="totalPages <= 5" class="flex justify-center gap-2">
+            <PaginationBtn v-for="page in totalPages" :key="page" :page-number="page" :is-active="page === currentPage"
+              :disabled="page === currentPage" @click="sliceData((page - 1) * pageSize, page * pageSize)"
+              @send-page-number="currentPage = $event" />
+          </div>
+          <div v-else class="flex justify-center gap-2">
+            <!-- previous -->
+            <div class="flex justify-center items-center" v-if="currentPage > 5">
+              <Icon name="iconamoon:arrow-left-2-fill" size="2rem" @click="prevPage" />
+            </div>
+            <PaginationBtn v-for="page in totalPages" :key="page" :page-number="page" :is-active="page === currentPage"
+              :disabled="page === currentPage" @click="sliceData((page - 1) * pageSize, page * pageSize)"
+              @send-page-number="currentPage = $event" />
+
+            <!-- next button -->
+            <div class="flex justify-center items-center" v-if="currentPage > 4">
+              <Icon name="iconamoon:arrow-right-2-fill" size="2rem" @click="nextPage" />
+            </div>
+          </div>
+        </div>
+
+      </div>
     </div>
   </div>
 </template>
