d511d388c23edbf9419d35ae19f65d0d6a5a591e
MinjaBaraka <barakaminja71@gmail.com>
2025-03-07 17:58:48 +0300

    modified:   pages/video/index.vue

--- pages/video/index.vue
+++ pages/video/index.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="container">
-    <HeroSection />
-    <TabBar />
-    <div class="loading flex justify-center" >
-      <LoadingIndicator :is-loading="true" />
+  <NuxtLayout name="home-layout">
+    <div class="container">
+      <HeroSection />
+      <TabBar />
+      <div class="loading flex justify-center" >
+        <LoadingIndicator :is-loading="true" />
+      </div>
+      <!-- render books -->
     </div>
-    <!-- render books -->
-  </div>
+  </NuxtLayout>
 </template>
 
 <script setup>
