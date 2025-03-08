a645f0c23459b2cb9c4d2b31350ee624c959faf9
MinjaBaraka <barakaminja71@gmail.com>
2025-03-08 17:02:26 +0300

    modified:   middleware/auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,10 @@ export default defineNuxtRouteMiddleware((to, from) => {
     const user = useCookie('userToken') // Example: Checking login token in cookies
     const path = useState('topicToView')
 
-    if (!user.value && path.value && to.path !== '/auth' ) {
+    if (!user.value && to.path !== '/auth' ) {
+       if(!path.value ){
+        return navigateTo('/home')
+       } 
       return navigateTo('/auth') 
     }
 });
