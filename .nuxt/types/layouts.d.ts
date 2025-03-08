1d20733c5d5263e9871db981aadf23fa431e5da4
MinjaBaraka <barakaminja71@gmail.com>
2025-03-08 08:24:45 +0300

    Removed .nuxt/ from Git tracking

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import type { ComputedRef, MaybeRef } from 'vue'
-export type LayoutKey = "course-details" | "home-layout"
-declare module "../../node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
-  }
-}
