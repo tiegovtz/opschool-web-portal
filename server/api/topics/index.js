8f2d2b38fd74e7a3ea5e0a26903f57015fa120f5
MinjaBaraka <barakaminja71@gmail.com>
2025-03-12 19:18:25 +0300

    modified:   pages/home/index.vue 	modified:   server/api/topics/index.js 	modified:   utilities/api-docs.js

--- server/api/topics/index.js
+++ server/api/topics/index.js
@@ -4,11 +4,29 @@ import apiDocs from "~/utilities/api-docs";
 
 
 export default defineEventHandler(async (event) => {
-   await axios.get(apiDocs.topics.getTopics).then((response)=>{
-    console.log('resp',response)
-    return response.data;
-   }).catch((error)=>{
-       console.log('error', error.message)
-     return error;
-   })
+    const auth_token = getCookie(event,'auth_token')
+
+    try {
+        const response = await fetch(apiDocs.topics.getTopics, {
+            method: 'GET',
+            headers: {
+                'Authorization': auth_token
+            }
+        })
+
+        const data = await response.json()
+
+        if (response.ok) {
+            console.log('resp', data,'token',auth_token)
+            return response.data; 
+        }
+        else{
+            return { error: 'unauthorized', 'token': auth_token }
+        }
+
+        
+    } catch (error) {
+        console.log('error', error.message)
+        return error;
+    }
 });
