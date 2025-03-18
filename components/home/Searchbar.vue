9380a0acb3ffd97bcbf7980795055d3dc165dee6
MinjaBaraka <barakaminja71@gmail.com>
2025-03-18 08:47:32 +0300

    modified:   assets/css/main.css 	modified:   components/home/InputsSelection.vue 	modified:   components/home/Searchbar.vue 	modified:   components/included/HeaderView.vue 	modified:   nuxt.config.ts

--- components/home/Searchbar.vue
+++ components/home/Searchbar.vue
@@ -10,18 +10,18 @@ const search = () => {
 <template>
          <!-- Search Form -->
     <form action="" @submit.prevent="search"
-      class="flex items-center w-full max-w-md focus:outline-none focus:ring-2 focus:ring-gray-400 mt-auto rounded-lg bg-gray-100 p-2">
+    class="flex w-full max-w-md h-10 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue">
       <div class="flex items-center w-full">
         <!-- Search Icon -->
         <Icon name="mdi:magnify" class="text-gray-400" size="1.5rem" />
 
         <!-- Search Input -->
-        <input type="text" placeholder="Search" class="flex-1 h-full focus:outline-none caret-gray-400 px-2" />
+        <input type="text" placeholder="Search" class="flex flex-1 h-full px-2 focus:outline-none focus:ring-0 focus:border-oceanBlue" />
       </div>
 
       <!-- Search Button -->
       <button type="submit"
-        class="bg-oceanBlue text-white p-2 rounded-lg hover:bg-blue-400 transition duration-500 cursor-pointer">
+        class="border-1 border-gray-300 hover:border-oceanBlue text-gray-400  hover:text-white p-2 rounded-t-md rounded-b-none hover:bg-oceanBlue transition duration-500 cursor-pointer">
         Search
       </button>
     </form>
--- components/included/HeaderView.vue
+++ components/included/HeaderView.vue
@@ -25,9 +25,9 @@ const dropDown = () => {
 
     <nav class="wrapper-container flex flex-col items-center">
       <!-- Phone Number -->
-      <div class="relative flex  items-center bg-oceanBlue w-full rounded-b-md text-white">
+      <div class="relative flex md:flex-row flex-col items-center bg-oceanBlue w-full rounded-b-md text-white">
 
-        <div class="title px-2 text-large font-medium uppercase text-shadow flex-1 text-center relative left-40">
+        <div class="title px-2 lg:text-large text-medium font-medium uppercase text-shadow flex-1 text-center relative xl:left-40">
           TIE online public school
         </div>
         <div class="info">
@@ -37,13 +37,13 @@ const dropDown = () => {
                 <NuxtImg v-if="userToken?.profilePic" :src="apiDocs.baseURL" alt="User Profile"
                   class="w-full h-full object-cover" />
                 <Icon v-else name="iconamoon:profile-circle-thin" class="" size="2rem" />
-                <p class="text-lg capitalize line-clamp-1 max-w-60">Hello, {{ userToken?.name ?
+                <p class="lg:text-large text-medium capitalize line-clamp-1 max-w-60">Hello, {{ userToken?.name ?
                   String(userToken.name).split(' ')[0]
                   :'friend' }}
                 </p>
               </div>
             </div>
-            <div class="flex items-center gap-2 cursor-pointer  text-white border-1 h-8 p-2 border-white rounded-md"
+            <div class="flex items-center gap-2 cursor-pointer  text-white border-1 md:h-8 h-6 p-2 border-white rounded-md"
               @click="logout">
               <Icon name="solar:logout-2-outline" class="" size="1.2rem" title="Sign out" />
               <span class="capitalize">
@@ -53,14 +53,14 @@ const dropDown = () => {
           </div>
           <div class="p-2 flex items-center gap-4" v-else>
             <NuxtLink to="/auth"
-              class="flex items-center gap-2 cursor-pointer  text-white border-1 h-8 px-1 border-white rounded-md">
+              class="flex items-center gap-2 cursor-pointer  text-white border-1  md:h-8 h-6 px-1 border-white rounded-md">
               <Icon name="solar:login-2-outline" class="" size="1.5rem" title="Sign in" />
               <span>Sign in </span>
             </NuxtLink>
 
             <!-- sign up -->
             <NuxtLink to="/auth/SignUp"
-              class="flex items-center gap-2 cursor-pointer  text-white border-1 h-8 px-1 border-white rounded-md">
+              class="flex items-center gap-2 cursor-pointer  text-white border-1 md:h-8 h-6 px-1 border-white rounded-md">
               <Icon name="iconamoon:profile-thin" class="" size="1.5rem" title="Sign in" />
               <span>Create Account</span>
             </NuxtLink>
@@ -70,15 +70,15 @@ const dropDown = () => {
       </div>
 
       <!-- Header -->
-      <div class="relative w-full h-19  mt-1 bg-[url('/public/flag/Flag_of_Tanzania.gif')] bg-cover bg-center bg-no-repeat">
+      <div class="relative w-full h-20  mt-1 bg-[url('/public/flag/Flag_of_Tanzania.gif')] bg-cover bg-center bg-no-repeat">
         <div class="absolute top-0 left-0 z-0 w-full h-full bg-[#f0f0f0e0]"></div>
         <div class="flex justify-between items-center w-full h-full absolute top-0 left-0  ">
           <div class="flex items-center justify-center bg-grayLight shadow-2xl shadow-grayLight h-full ">
           <NuxtImg src="/logo/emblem.webp" alt="EMBLEM" class="w-18 h-18" />
         </div>
         <div class="title flex flex-col items-center text-center gap-1  h-full">
-          <p class="text-medium  md:text-large font-bold uppercase text-shadow">wizara ya elimu, sayansi na teknolojia </p>
-          <p class="text-small md:text-medium capitalize">taasisi ya elimu tanzania (TET)</p>
+          <p class="lg:text-large md:text-medium text-extraSmall font-bold uppercase text-shadow">wizara ya elimu, sayansi na teknolojia </p>
+          <p class="lg:text-medium md:text-small text-smallest capitalize">taasisi ya elimu tanzania (TET)</p>
         </div>
         <NuxtLink to="/" class="cursor-pointer p-2 h-full bg-grayLight shadow-2xl shadow-grayLight ">
           <NuxtImg src="/logo/logo_tie.webp" alt="TIE LOGO" class="w-14 h-14" />
