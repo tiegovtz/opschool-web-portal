07e6cf260a7b1e281d71ffb2ca8c2e6a79d0ecef
MinjaBaraka <barakaminja71@gmail.com>
2025-03-21 19:27:54 +0300

    new file:   pages/error/access-denied.vue 	modified:   server/api/video/[videoId].ts

---
+++ pages/error/access-denied.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex items-center justify-center h-dvh">
+        <div class="">
+            <p class="text-center text-medium ">403</p>
+            <p class="">Access Denied</p>
+            <nuxt-link to="/">Go Home</nuxt-link>
+        </div>
+    </div>
+</template>
