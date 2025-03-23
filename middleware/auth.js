1e633aaf7a9710e7555869204de14ac0712e26d7
Cyden-Mnara <mnarasaid7@gmail.com>
2025-03-23 21:09:33 +0300

    modified:   middleware/auth.js 	deleted:    middleware/token.js 	modified:   pages/interactive/[level]/[subject]/[topic]/[topicId].vue

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,20 @@
-import { isTokenExpiringSoon } from "~/utilities/jwToken";
+import { refreshToken ,isTokenExpiringSoon } from "~/utilities/jwToken";
 
-export default defineNuxtRouteMiddleware((to, from) => {
-    const user = useCookie('signInUserToken') // Example: Checking login token in cookies
-    const path = useState('topicToView')
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  const user = useCookie("signInUserToken"); // Example: Checking login token in cookies
+  const path = useState("topicToView");
 
-    
-
-    if (!user.value && to.path !== '/auth' && isTokenExpiringSoon(user.value,300) ) {
-       if(!path.value ){
-        return navigateTo('/home')
-       } 
-      return navigateTo('/auth') 
+  if (!user.value && to.path !== "/auth") {
+    if (!path.value) {
+      return navigateTo("/home");
     }
+    return navigateTo("/auth");
+  } else if (isTokenExpiringSoon(token.value, 60)) {
+    newToken = await refreshToken();
+    user.value = newToken?.access_token;
+  }
+
+  else{
+    return navigateTo("/auth");
+  }
 });
