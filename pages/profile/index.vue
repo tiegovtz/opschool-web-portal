9ed8211ba9187a7686d353b6e975ddefe8f1d229
MinjaBaraka <barakaminja71@gmail.com>
2025-04-15 19:24:50 +0300

    7:24PM
    Not complete profile UI/UX

---
+++ pages/profile/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+import  ProfileCard  from "@/components/profile/profileCard.vue";
+
+</script>
+
+<template>
+    <NuxtLayout name="home-layout">
+        <section class="flex items-center justify-center py-6">
+            <ProfileCard />
+        </section>
+    </NuxtLayout>
+</template>
