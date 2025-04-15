336e579e82398ed52fbc6f42ffe3ef9a526907d3
Cyden-Mnara <mnarasaid7@gmail.com>
2025-04-15 20:03:02 +0300

    modified:   utilities/parsers/videoParser.ts

--- utilities/parsers/videoParser.ts
+++ utilities/parsers/videoParser.ts
@@ -7,11 +7,29 @@ const videoParser = (query: string): string => {
     return query.replace(regex, (match, videoAttrs, sourceSrc) => {
         return `
             <video id='video-player' ${videoAttrs} controls controlslist="nodownload" oncontextmenu="return false;" preload="auto">
-              <source src="${`/api/video/`+sourceSrc}" type="application/x-mpegURL">
+              <source src="${`/api/video/`+sourceSrc}" type="video/mp4">
               Your browser does not support the video tag.
             </video>
         `;
     });
 }
 
-export default videoParser;
+// export default videoParser;
+
+// // import apiDocs from "../api-docs";
+
+// const videoParser = (query: string): string => {
+//     // Regular expression to match <video> tags with a <source> inside
+//     const regex = /<video\b([^>]*)>\s*<source\s+src="([^"]+)"(?:\s+type="([^"]+)")?\s*>\s*<\/video>/gi;
+
+//     return query.replace(regex, (match, videoAttrs, sourceSrc) => {
+//         return `
+//             <video id='video-player' ${videoAttrs} controls controlslist="nodownload" oncontextmenu="return false;" preload="auto">
+//               <source src="${`/api/video/`+sourceSrc}" type="application/x-mpegURL">
+//               Your browser does not support the video tag.
+//             </video>
+//         `;
+//     });
+// }
+
+// export default videoParser;
