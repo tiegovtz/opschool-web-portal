54b30e496264df114c68f4fce9a837121d99eda4
MinjaBaraka <barakaminja71@gmail.com>
2025-04-28 19:46:53 +0300

    7:46PM

--- components/included/HeaderView.vue
+++ components/included/HeaderView.vue
@@ -3,9 +3,15 @@ import apiDocs from "~/utilities/api-docs";
 import { layoutEffect } from "~/utilities/controlls";
 
 const userToken = useCookie("signInUserToken");
+const accessToken = useCookie("signInAccessToken");
+const refreshToken = useCookie("signInRefreshToken");
 
 const logout = () => {
+  // Clear All Cookies
   userToken.value = null;
+  accessToken.value = null;
+  refreshToken.value = null;
+  
   const router = useRouter();
   layoutEffect.value = "grid";
   // Use the State
