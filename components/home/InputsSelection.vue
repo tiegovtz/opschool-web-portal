a784e2d6ce65ad24cfcd78288a3e487ebec48ecd
MinjaBaraka <barakaminja71@gmail.com>
2025-04-28 08:54:52 +0300

    28 April 2025

--- components/home/InputsSelection.vue
+++ components/home/InputsSelection.vue
@@ -34,10 +34,10 @@ const sendEmits =()=>{
     </select>
 
 
-    <!-- Primary School Level Selection -->
+    <!-- Primary School Class Level Selection -->
     <select v-model="standard" v-if="level === 'primary'" name="" id="" @change="sendEmits"
       class="w-full h-10 px-2 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue">
-      <option value="">Select Standard Level</option>
+      <option value="">Select Class</option>
       <option value="Standard1">Standard 1</option>
       <option value="Standard2">Standard 2</option>
       <option value="Standard3">Standard 3</option>
@@ -49,7 +49,7 @@ const sendEmits =()=>{
     <!-- Secondary School Level Selection -->
     <select v-model="standard" v-else-if="level === 'secondary' || level === 'all'" name="" id="" @change="sendEmits"
       class="w-full h-10 px-2 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue">
-      <option value="">Select Ordinary Level</option>
+      <option value="">Select Class</option>
       <option value="Form 1">Form 1</option>
       <option value="Form 2">Form 2</option>
       <option value="Form 3">Form 3</option>
@@ -59,7 +59,7 @@ const sendEmits =()=>{
     <!-- High School Level Selection -->
     <select v-model="standard" v-else-if="level === 'high'" name="" id="" @change="sendEmits"
       class="w-full h-10 px-2 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue">
-      <option value="">Select Advance Level</option>
+      <option value="">Select Class</option>
       <option value="Form 5">Form 5</option>
       <option value="Form 6">Form 6</option>
     </select>
@@ -69,16 +69,10 @@ const sendEmits =()=>{
       class="w-full h-10 px-2 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue">
       <option value="">Select Subject</option>
       <option value="Physics">Physics</option>
+      <option value="Mathematics">Mathematics</option>
       <option value="Chemistry">Chemistry</option>
       <option value="Biology">Biology</option>
       <option value="Geography">Geography</option>
-      <option value="Mathematics">Mathematics</option>
-      <option value="English">English</option>
-      <option value="French">French</option>
-      <option value="Spanish">Spanish</option>
-      <option value="German">German</option>
-      <option value="Italian">Italian</option>
-      <option value="Portuguese">Portuguese</option>
     </select>
   </form>
 
