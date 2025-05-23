f8407e0adca5afb284d421d76f920135c680e593
MinjaBaraka <barakaminja71@gmail.com>
2025-05-23 17:53:45 +0300

    4:53PM

--- components/home/TopicCard.vue
+++ components/home/TopicCard.vue
@@ -93,7 +93,7 @@ const userToken = useCookie('signInUserToken')
     :to="`/interactive/${topicStandard}/${subjectName}/${topicTitle}/${topicId}`"
     @click="setTopicToView()" :class="[
       'relative flex overflow-hidden rounded-lg shadow-md group transition-all duration-500 ease-in-out min-w-[300px]',
-      layoutEffect == 'grid' && modelType === 'card' ? 'flex-col' : 'flex-row h-32',
+      layoutEffect == 'grid' && modelType === 'card' ? 'flex-col lg:pb-4' : 'flex-row h-32',
       { 'cursor-pointer flex-row my-2 pb-0 md:h-20 !max-h-14 ': modelType === 'search' },
     ]">
     <!-- topic image -->
