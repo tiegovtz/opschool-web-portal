15ba4c35004e8c97bdaac906071ef913c41cf3a6
MinjaBaraka <barakaminja71@gmail.com>
2025-05-27 17:36:43 +0300

    5:37PM
    Updated

---
+++ components/home/SearchResults.vue
@@ -0,0 +1,79 @@
+<script setup>
+// Define Stores
+const navigationStore = useNavigationStore()
+
+// Define Props
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    default: '/images/background2.webp',
+  },
+  level: {
+    type: String,
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+});
+
+const pageToView =  computed(() => {
+let page;
+if (props.type.toLocaleLowerCase() === 'topic') {
+    page = 'interactive';
+  } else if (props.type.toLocaleLowerCase() === 'activity') {
+    page = 'experiment';
+  } else if (props.type.toLocaleLowerCase() === 'video') {
+    page = 'video';
+  } else if (props.type.toLocaleLowerCase() === 'audio') {
+    page = 'audio';
+  } else {
+    page = 'topic';
+  }
+  return page;
+});
+
+const setPageToView = () => {
+    if (props.type.toLocaleLowerCase() === 'topic') {
+        navigationStore.setTopic(`/${pageToView.value}/${props.level}/${props.subject}/${props.title}/${props.id}`)
+    }
+    if (props.type.toLocaleLowerCase() === 'activity') {
+        navigationStore.setExperiment(`/${pageToView.value}/${props.level}/${props.subject}/${props.title}/${props.id}`)
+    }
+    if (props.type.toLocaleLowerCase() === 'video') {
+        navigationStore.setVideo(`/${pageToView.value}/${props.level}/${props.subject}/${props.title}/${props.id}`)
+    }
+    if (props.type.toLocaleLowerCase() === 'audio') {
+        navigationStore.setAudio(`/${pageToView.value}/${props.level}/${props.subject}/${props.title}/${props.id}`)
+    }
+}
+</script>
+
+<template>
+  <NuxtLink
+  @click="setPageToView()"
+  :to="`/${pageToView}/${level}/${subject}/${title}/${id}`"
+  class="flex items-center p-4 transition-shadow duration-500 ease-in-out bg-white rounded-lg shadow-md hover:shadow-lg">
+    <div class="max-w-[100px] h-20 mb-4 overflow-hidden rounded-md">
+      <NuxtImg :src="thumbnail" loading="lazy" :alt="'Image of ' + thumbnail" class="object-cover w-full h-full" />
+    </div>
+   <div class="flex flex-col justify-between flex-1 ml-4">
+    <h2 class="mb-2 text-xl font-semibold">{{ title }}</h2>
+    <p class="mb-2 text-gray-600"> {{ level }}</p>
+    <p class="mb-2 text-gray-600"> {{ subject }}</p>
+   </div>
+  </NuxtLink>
+</template>
--- components/home/Searchbar.vue
+++ components/home/Searchbar.vue
@@ -1,102 +1,183 @@
 <script setup>
-import axios from 'axios';
-import TopicCard from './TopicCard.vue';
+import axios from "axios";
+import TopicCard from "./TopicCard.vue";
 import apiDocs from "~/utilities/api-docs";
+import SearchResults from "./SearchResults.vue";
+
+const userToken = useCookie("signInUserToken");
 
 const searchReactive = reactive({
   search: null,
-  searchResult: null
-})
+  searchResult: null,
+});
 
 const search = async () => {
-  await axios.get(`${apiDocs.topics.filterTopics}?name=${searchReactive.search.trim()}`)
+  await axios
+    .get(
+      userToken.value
+        ? `${apiDocs.search.getSearch}?query=${searchReactive.search.trim()}`
+        : `${apiDocs.topics.filterTopics}?name=${searchReactive.search.trim()}`,
+        
+        {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${useCookie("signInAccessToken").value}`,
+      }
+        }
+    )
     .then((response) => {
       searchReactive.searchResult = response.data;
     })
     .catch((error) => {
       searchReactive.searchResult = null;
-    })
-}
+    });
+};
 
 defineProps({
   appearance: {
     type: String,
-    default: 'normal'
+    default: "normal",
   }
-})
+});
 
 // watch search
 const inputSearch = (event) => {
   const newVal = event.target.value;
 
-  if (newVal && newVal.trim() !== '') {
+  if (newVal && newVal.trim() !== "") {
     search();
   } else {
     searchReactive.searchResult = null;
   }
-}
+};
 </script>
 
 <template>
-  <div :class="[
-    ' flex items-center justify-center w-full',
-    appearance === 'normal' ? 'max-w-md' : `md:h-72 h-32 bg-background3 bg-cover bg-center bg-no-repeat rounded-md`
-  ]">
-    <div :class="[
-      ' relative flex items-center justify-center w-full h-full rounded-md',
-      appearance === 'normal' ? 'md:px-0 lg:px-0' : 'bg-textGray bg-opacity-40 md:px-10 lg:px-[100px] p-1'
-    ]">
+  <div
+    :class="[
+      ' flex items-center justify-center w-full',
+      appearance === 'normal'
+        ? 'max-w-md'
+        : `md:h-72 h-32 bg-background3 bg-cover bg-center bg-no-repeat rounded-md`,
+    ]"
+  >
+    <div
+      :class="[
+        ' relative flex items-center justify-center w-full h-full rounded-md',
+        appearance === 'normal'
+          ? 'md:px-0 lg:px-0'
+          : 'bg-textGray bg-opacity-40 md:px-10 lg:px-[100px] p-1',
+      ]"
+    >
       <!-- Apperance Normal -->
-      <form v-if="appearance === 'normal'" action="" @submit.prevent="search"
-        class="flex w-full h-10 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue">
+      <form
+        v-if="appearance === 'normal'"
+        action=""
+        @submit.prevent="search"
+        class="flex w-full h-10 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue"
+      >
         <div class="flex items-center w-full">
           <!-- Search Icon -->
           <Icon name="mdi:magnify" class="text-gray-400" size="1.5rem" />
 
           <!-- Search Input -->
-          <input type="text" @input="inputSearch" v-model="searchReactive.search"
+          <input
+            type="text"
+            @input="inputSearch"
+            v-model="searchReactive.search"
             placeholder="What do you want to learn?"
-            class="flex flex-1 h-full px-2 focus:outline-none focus:ring-0 focus:border-oceanBlue" />
+            class="flex flex-1 h-full px-2 focus:outline-none focus:ring-0 focus:border-oceanBlue"
+          />
         </div>
 
         <!-- Search Button -->
-        <button type="submit" @click="search"
-          class="items-center justify-center hidden px-4 py-2 overflow-hidden text-white transition-colors duration-500 ease-in-out rounded-b-none cursor-pointer md:flex rounded-t-md bg-oceanBlue hover:bg-deepBlue">
+        <button
+          type="submit"
+          @click="search"
+          class="items-center justify-center hidden px-4 py-2 overflow-hidden text-white transition-colors duration-500 ease-in-out rounded-b-none cursor-pointer md:flex rounded-t-md bg-oceanBlue hover:bg-deepBlue"
+        >
           Search
         </button>
       </form>
 
       <!-- Apperance Not Normal -->
-      <form v-else action="" class="flex items-center w-full max-w-3xl p-2 bg-white rounded-md h-15"
-        @submit.prevent="search">
+      <form
+        v-else
+        action=""
+        class="flex items-center w-full max-w-3xl p-2 bg-white rounded-md h-15"
+        @submit.prevent="search"
+      >
         <div class="flex items-center w-full pl-4">
           <!-- Search Icon -->
           <Icon name="mdi:magnify" class="text-gray-400" size="1.5rem" />
 
           <!-- Search Input -->
-          <input type="text" @input="inputSearch" v-model="searchReactive.search"
+          <input
+            type="text"
+            @input="inputSearch"
+            v-model="searchReactive.search"
             placeholder="What do you want to learn?"
-            class="flex flex-1 h-full px-2 focus:outline-none focus:ring-0 focus:border-oceanBlue" />
+            class="flex flex-1 h-full px-2 focus:outline-none focus:ring-0 focus:border-oceanBlue"
+          />
         </div>
 
         <!-- Search Button -->
-        <button type="submit" @click="search"
-          class="items-center justify-center hidden h-full px-4 py-2 overflow-hidden text-white transition-colors duration-500 ease-in-out rounded-b-none cursor-pointer md:flex rounded-r-md bg-oceanBlue hover:bg-deepBlue">
+        <button
+          type="submit"
+          @click="search"
+          class="items-center justify-center hidden h-full px-4 py-2 overflow-hidden text-white transition-colors duration-500 ease-in-out rounded-b-none cursor-pointer md:flex rounded-r-md bg-oceanBlue hover:bg-deepBlue"
+        >
           Search
         </button>
       </form>
-      <!-- Result Search -->
-      <div v-if="searchReactive.searchResult && searchReactive.search" :class="[
-        'absolute z-50  w-full bg-white shadow-md rounded-md max-h-[400px] overflow-y-scroll',
-        appearance === 'normal' ? 'top-10 left-0 max-w-md' : 'top-[96px] max-w-3xl px-1'
-      ]">
-        <TopicCard v-for="result in searchReactive.searchResult" :key="result._id" model-type="search"
-          :topic-id="result._id" :topic-title="result.name" :topic-image="result.thumbnail"
-          :topic-standard="result.standard" :topic-subject="result.subject.name"
-          :topic-description="result.descriptions" :topic-level="result.level.name" :topic-likes="0"
-          :topic-views="topic?.viewedBy?.length ? topic?.viewedBy?.length : 0" topic-duration="0" />
+      <!-- Result Search with NO userToken -->
+      <div
+        v-if="searchReactive.searchResult && searchReactive.search && !userToken "
+        :class="[
+          'absolute z-50  w-full bg-white shadow-md rounded-md max-h-[400px] overflow-y-scroll',
+          appearance === 'normal'
+            ? 'top-10 left-0 max-w-md'
+            : 'top-[96px] max-w-3xl px-1',
+        ]"
+      >
+        <TopicCard
+          v-for="result in searchReactive.searchResult"
+          :key="result._id"
+          model-type="search"
+          :topic-id="result._id"
+          :topic-title="result.name"
+          :topic-image="result.thumbnail"
+          :topic-standard="result.standard"
+          :topic-subject="result.subject.name"
+          :topic-description="result.descriptions"
+          :topic-level="result.level.name"
+          :topic-likes="0"
+          :topic-views="topic?.viewedBy?.length ? topic?.viewedBy?.length : 0"
+          topic-duration="0"
+        />
+      </div>
+
+      <!-- Result Search with userToken -->
+      <div
+        v-else-if="searchReactive.searchResult && searchReactive.search && userToken "
+        :class="[
+          'absolute z-50  w-full bg-white shadow-md rounded-md max-h-[400px] overflow-y-scroll',
+          appearance === 'normal'
+            ? 'top-10 left-0 max-w-md'
+            : 'top-[180px] max-w-3xl px-1',
+        ]"
+      >
+       <SearchResults
+          v-for="result in searchReactive.searchResult"
+          :key="result._id"
+          :id="result._id"
+          :title="result.name"
+          :thumbnail="result.thumbnail"
+          :level="result?.level ?? 'Form 1'"
+          :subject="result?.subject ?? 'N/A'"
+        />
+        
       </div>
     </div>
   </div>
-
 </template>
