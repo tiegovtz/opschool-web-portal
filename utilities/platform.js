bada5fb92a9c0d0810ea2e3f4b3bc1df02f28fcd
MinjaBaraka <barakaminja71@gmail.com>
2025-05-27 13:24:42 +0300

    27 May 2025 1:24PM
    Updated

---
+++ utilities/platform.js
@@ -0,0 +1,24 @@
+import Bowser from "bowser";
+import apiDocs from "./api-docs";
+
+export const webVisitor = async () => {
+  // Get platform details from Bowser
+  const browser = Bowser.getParser(window.navigator.userAgent);
+  const parsed = browser.parse();
+
+  const browserName = parsed.getBrowserName() || "Unknown Browser";
+  const osName = parsed.getOSName() || "Unknown OS";
+
+  try {
+     await $fetch(apiDocs.visitors.postVisitors, {
+      method: "POST",
+      body: {
+        browser: browserName,
+        os: osName,
+      },
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+};
