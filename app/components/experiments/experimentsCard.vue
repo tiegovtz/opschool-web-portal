c864f5fe2d9d97c3d0fe3f4ca09e49cddf3b74bb
aloycius <aloyciuslaurent58@gmail.com>
2025-12-03 09:51:40 +0300

    made changes: to experimentsCard.vue and videoCard.vue

--- app/components/experiments/experimentsCard.vue
+++ app/components/experiments/experimentsCard.vue
@@ -65,7 +65,7 @@ const setExperimentUrl =()=>{
     <NuxtLink
         :to="`/experiments/${experimentStandard}/${experimentSubject}/${experimentName}/${experimentId}`" 
         @click="setExperimentUrl()"
-        :aria-label="`View ${experimentName} experiment`"
+        :aria-label="`View ${experimentName} experiment with experiment description ${experimentDescription}`"
         :class="[
             'relative flex overflow-hidden transition-all duration-500 ease-in-out bg-white rounded-lg shadow-md cursor-pointer hover:bg-deepBlue hover:shadow-xl group min-w-[300px]',
             layoutEffect == 'grid' ? 'flex-col h-[350px]' : 'flex-row h-32'
