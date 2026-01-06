1db7b96558e006ce319c8a3831d3949a3b09e03f
Cyden-Mnara <mnarasaid7@gmail.com>
2026-01-06 12:40:23 +0300

    external filter : 	modified:   app/components/home/InputsSelection.vue 	modified:   app/types/classlevel.interface.ts 	modified:   app/utilities/apiDocs.ts

--- app/components/home/InputsSelection.vue
+++ app/components/home/InputsSelection.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+import type { ClassLevel } from '~/types/classlevel.interface'
+import type { educationLevel } from '~/types/educationlevel.interface'
+import type { Subjects } from '~/types/subject.interface'
+import apiDocs from '~/utilities/apiDocs'
+
 const level = ref<string>('')
 const standard = ref<string>('')
 const subject = ref<string>('')
@@ -11,12 +16,24 @@ const emit = defineEmits([
   'emitStandard'
 ])
 
-const sendEmits =()=>{
+const sendEmits = () => {
   emit('emitLevel', level.value)
   emit('emitStandard', standard.value)
   emit('emitSubject', subject.value)
 
 }
+
+// Auth headers
+const token = useCookie("signInAccessToken").value;
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${token}`,
+};
+
+// Server data
+const { data: educationLevels } = useFetch<educationLevel[]>(apiDocs.educationLevel.getEducationLevels, { headers });
+const { data: classes } = useFetch<ClassLevel[]>(apiDocs.levels.getLevels, { headers });
+const { data: subjects } = useFetch<Subjects[]>(apiDocs.subjects.getPublicSubjects, { headers });
 
 
 </script>
@@ -24,76 +41,47 @@ const sendEmits =()=>{
 <template>
   <div class="flex flex-col items-center justify-between gap-4 px-4 my-5 xl:flex-row">
     <form action="" class="flex flex-col items-center justify-center w-full gap-4 my-5 md:flex-row">
-    <select v-model="level" name="" id=""
-      class="w-full h-10 px-2 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue"
-      @change="sendEmits">
-      <option value="">Select Level</option>
-      <option value="secondary">Lower Secondary</option>
-      <option value="high">Upper Secondary</option>
-      <option value="teacher">Teacher Education</option>
-    </select>
+      <select v-model="level" name="educationLevel" id=""
+        class="w-full h-10 px-2 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue"
+        @change="sendEmits">
+        <option v-if="educationLevels" value="">Select level</option>
+        <option v-else value="">loading .. </option>
+        <option v-for="(level, i) in educationLevels" :value="level.name.toLowerCase()"
+          :key="`education-level-option-${level._id}-${i}`">{{ level.name }}</option>
+      </select>
 
+      <!-- Secondary School Level Selection -->
+      <select v-model="standard" v-if="level.trim()" name="classLevel" id="" @change="sendEmits"
+        class="w-full h-10 px-2 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue">
+        <option v-if="classes" value="">Select class</option>
+        <option v-else value="">loading .. </option>
+        <option v-for="(cls, i) in classes?.filter(c => c.educationLevel.name.toLowerCase() === level)" :value="cls.name"
+          :key="`class-level-option-${cls._id}-${i}`">{{ cls.name
+          }}</option>
+      </select>
+      <!-- Level Selection -->
+      <select v-model="standard" v-else name="classLevel" id="" @change="sendEmits"
+        class="w-full h-10 px-2 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue">
+        <option value="">Select Level First</option>
+      </select>
 
-    <!-- Primary School Class Level Selection -->
-    <!-- <select v-model="standard" v-if="level === 'primary'" name="" id="" @change="sendEmits"
+      <!-- Subject -->
+      <select name="" v-if="level.trim() && standard.trim()" v-model="subject" id="" @change="sendEmits"
+        class="w-full h-10 px-2 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue">
+        <option v-if="subjects" value="">Select subject</option>
+        <option v-else value="">loading .. </option>
+        <option v-for="(sbj, i) in subjects" :value="sbj.name"
+          :key="`subjects-option-${sbj._id}-${i}`">{{ sbj.name
+          }}</option>
+      </select>
+
+      <!-- Subject -->
+      <select name="" v-else v-model="subject" id="" @change="sendEmits"
       class="w-full h-10 px-2 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue">
-      <option value="">Select Class</option>
-      <option value="Standard1">Standard 1</option>
-      <option value="Standard2">Standard 2</option>
-      <option value="Standard3">Standard 3</option>
-      <option value="Standard4">Standard 4</option>
-      <option value="Standard5">Standard 5</option>
-      <option value="Standard6">Standard 6</option>
-    </select> -->
-
-    <!-- Secondary School Level Selection -->
-    <select v-model="standard" v-if="level === 'secondary' || level === 'all'" name="" id="" @change="sendEmits"
-      class="w-full h-10 px-2 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue">
-      <option value="">Select Class</option>
-      <option value="Form 1">Form 1</option>
-      <option value="Form 2">Form 2</option>
-      <option value="Form 3">Form 3</option>
-      <option value="Form 4">Form 4</option>
+      <option value="">Select class first</option>
     </select>
+    </form>
 
-    <!-- High School Level Selection -->
-    <select v-model="standard" v-else-if="level === 'high'" name="" id="" @change="sendEmits"
-      class="w-full h-10 px-2 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue">
-      <option value="">Select Class</option>
-      <option value="Form 5">Form 5</option>
-      <option value="Form 6">Form 6</option>
-    </select>
-
-    <!-- Teacher Level Selection -->
-     <select v-model="standard" v-else-if="level === 'teacher'" name="" id="" @change="sendEmits"
-      class="w-full h-10 px-2 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue">
-      <option value="">Teacher Education</option>
-    </select>
-     <!-- Level Selection -->
-    <select v-model="standard" v-else name="" id="" @change="sendEmits"
-      class="w-full h-10 px-2 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue">
-      <option value="">Select Level First</option>
-    </select>
-
-    <!-- Subject -->
-    <select name="" v-if="level === 'secondary'"  v-model="subject" id="" @change="sendEmits"
-      class="w-full h-10 px-2 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue">
-      <option value="">Select Subject</option>
-      <option value="Physics">Physics</option>
-      <option value="Mathematics">Mathematics</option>
-      <option value="Chemistry">Chemistry</option>
-      <option value="Biology">Biology</option>
-      <!-- <option value="Geography">Geography</option> -->
-    </select>
-
-     <!-- Subject -->
-    <select name="" v-else v-model="subject" id="" @change="sendEmits"
-      class="w-full h-10 px-2 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-oceanBlue">
-      <option value="">Select Subject</option>
-    </select>
-  </form>
-
-   <HomeSearchbar />
+    <HomeSearchbar />
   </div>
 </template>
-
