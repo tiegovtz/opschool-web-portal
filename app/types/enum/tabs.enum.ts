d358425c4c385115f65476f4f7a91797225f1f3e
Cyden-Mnara <mnarasaid7@gmail.com>
2026-01-08 18:36:01 +0300

    renaming : 	modified:   app/assets/css/main.css 	modified:   app/pages/interactive/[subject]/[level]/[topic]/[topicId].vue 	modified:   app/types/enum/tabs.enum.ts

--- app/types/enum/tabs.enum.ts
+++ app/types/enum/tabs.enum.ts
@@ -1,7 +1,7 @@
 export enum HomeTabs{
     video="Videos",
     classVideos="Class Videos",
-    activity="Learn Activities",
+    activity="Learner's Activities",
     audio="Audios",
     smartClass="Smart Class",
     subject="Subjects",
