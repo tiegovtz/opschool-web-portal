4d8671da9a8e5506cec040aea15c044ea6ad5386
king-tiff <ngajatif313@gmail.com>
2026-01-13 12:22:09 +0300

    removed supabase dependancy on the DB

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,10 +26,6 @@ export default defineNuxtConfig({
   runtimeConfig: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     openaiApiKey: process.env.OPENAI_API_KEY || "",
-    public: {
-      supabaseUrl: process.env.NUXT_SUPABASE_URL,
-      supabaseKey: process.env.NUXT_SUPABASE_ANON_KEY,
-    },
   },
 
   plugins: [
@@ -44,7 +40,6 @@ export default defineNuxtConfig({
     "@nuxt/image",
     "@nuxt/icon",
     "nuxt-swiper",
-    "@nuxtjs/supabase",
   ],
 
   image: {
@@ -87,14 +82,6 @@ export default defineNuxtConfig({
       meta: [{ name: "generator", content: "" }],
     },
   },
-  supabase: {
-    redirect: false,
-    url: process.env.NUXT_SUPABASE_URL,
-    key: process.env.NUXT_SUPABASE_ANON_KEY,
-  
-  },
-  
-
 });
 
 //
