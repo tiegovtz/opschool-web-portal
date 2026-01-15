b565403d939f93ecfd8e8aec70bbe6b35bd5c53d
Cyden-Mnara <mnarasaid7@gmail.com>
2026-01-15 11:08:01 +0300

    Removed allowedHosts from nuxt config: 	modified:   nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -72,9 +72,6 @@ export default defineNuxtConfig({
   },
 
   vite: {
-    server:{
-      allowedHosts:['.local','localhost','opschool.tie.go.tz']
-    },
     css: {
       preprocessorOptions: {
         sass: {
